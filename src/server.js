62e8e97203e97efd586bdd62ea81840424099ca9
aul14 <rahmanaulia336@gmail.com>
2022-03-15 14:16:08 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -3,14 +3,22 @@ require('dotenv').config();
 
 //import nilai notes plugin dan NotesService
 const Hapi = require('@hapi/hapi');
+
+//notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 
 const init = async () => {
   //buat instance dari NotesService dengan nama notesService
   const notesService = new NotesService();
+  const usersService = new UsersService();
   /** daftarkan plugin notes dengan options.service bernilai notesService menggunakan 
    * perintah await server.register tepat sebelum kode await server.start() */
 
@@ -24,13 +32,22 @@ const init = async () => {
     },
   });
  
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
  
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
