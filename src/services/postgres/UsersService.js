de4e72d9416d80e7560bbe61f265b3bcfa3af59d
aul14 <rahmanaulia336@gmail.com>
2022-03-15 17:39:46 +0700

    add setengah authentication

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
  
 class UsersService {
   constructor() {
@@ -58,7 +59,32 @@ class UsersService {
 
     if (result.rows.length > 0) {
         throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.')
-      }
+    }
+  }
+
+  //membuat fungsi kredensial atau username dan password yang dikirimkan oleh pengguna benar atau tidak
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah.')
+    }
+
+    //Untuk nilai password, kita tampung ke variabel hashedPassword
+    const { id, password: hashedPassword } = result.rows[0];
+
+    //fungsi bcrypt.compare untuk mencocokan password dari user dan database
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
   }
 }
 module.exports = UsersService;
